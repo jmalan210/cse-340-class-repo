34613ea2660c729b1cefb7e4acd2c824d0474e98
jmalan210 <>
2026-03-10 11:33:40 -0600

    Learning Activity: Connecting to Databases from Node.js complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
 
 
 // Define the the application environment
@@ -57,7 +58,13 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+     
 });
---
+++ src/models/setup.sql
@@ -0,0 +1,15 @@
+-- Organization Table
+
+Create Table organization (
+organization_id SERIAL Primary Key,
+name varchar(150) not null,
+description text not null,
+contact_email varchar(255) not null, 
+logo_filename varchar (255) not null
+);
+
+insert into organization (name, description, contact_email, logo_filename)
+values
+('BrightFuture Builders', 'A nonprofit focused on improving community infrastructure through sustainable construction projects.', 'info@brightfuturebuilders.org', 'brightfuture-logo.png'),
+('GreenHarvest Growers', 'An urban farming collective promoting food sustainability and education in local neighborhoods.', 'contact@greenharvest.org', 'greenharvest-logo.png'),
+('UnityServe Volunteers', 'A volunteer coordination group supporting local charities and service initiatives.', 'hello@unityserve.org', 'unityserve-logo.png');
