60f87735063e7b7e4d11e7b2aee711b287f0a160
jmalan210 <>
2026-03-16 20:15:20 -0600

    middleware exercise

--- server.js
+++ server.js
@@ -31,6 +31,14 @@ app.set('view engine', 'ejs');
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
 app.use((req, res, next) => {
     console.log(req.method, req.url);
     res.locals.appName = 'Partner Portal';
