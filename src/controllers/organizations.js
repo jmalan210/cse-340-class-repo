9e787700220092326b8ea837ae207b1d24b5527b
jmalan210 <>
2026-03-16 22:03:35 -0600

    Route and Query Parameters activity done

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,4 +1,5 @@
-import {getAllOrganizations} from '../models/organizations.js'
+import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js'
+import { getProjectsByOrganizationId } from '../models/projects.js';
 
 const showOrganizationsPage = async (req, res) => {
     const organizations = await getAllOrganizations();
@@ -6,4 +7,32 @@ const showOrganizationsPage = async (req, res) => {
     res.render('organizations', { title, organizations });
 };
 
-export {showOrganizationsPage}
+const getOrganizationDetails = async (organizationId) => {
+      const query = `
+      SELECT
+        organization_id,
+        name,
+        description,
+        contact_email,
+        logo_filename
+      FROM organization
+      WHERE organization_id = $1;
+    `;
+
+      const query_params = [organizationId];
+      const result = await db.query(query, query_params);
+
+      // Return the first row of the result set, or null if no rows are found
+      return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+const showOrganizationDetailsPage = async (req, res) => {
+    const organizationId = req.params.id;
+    const organizationDetails = await getOrganizationDetails(organizationId);
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Organization Details';
+
+    res.render('organization', {title, organizationDetails, projects});
+};
+
+export {showOrganizationsPage, getOrganizationDetails, showOrganizationDetailsPage}
--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -7,4 +7,24 @@ const showProjectsPage = async (req, res) => {
     res.render('projects', { title, service_projects });
 };
 
-export {showProjectsPage}
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const query_params = [organizationId];
+      const result = await db.query(query, query_params);
+
+      return result.rows;
+};
+
+export {showProjectsPage, getProjectsByOrganizationId}
